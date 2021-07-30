8a29fae3d7123991e404e4dad76482b4235aa39d
Rich <richard.clements@image-plus.co.uk>
2021-07-30 07:46:52 +0100

    Update Request.swift

--- Sources/Data/Network/Request/Request.swift
+++ Sources/Data/Network/Request/Request.swift
@@ -14,12 +14,20 @@ public struct AuthenticatedRequest: Request {
     public let urlRequest: URLRequest
     public let requiresAuthentication = true
     
+    public init(urlRequest: URLRequest) {
+        self.urlRequest = urlRequest
+    }
+    
 }
 
 public struct PublicRequest: Request {
     
     public let urlRequest: URLRequest
     public let requiresAuthentication = false
+    
+    public init(urlRequest: URLRequest) {
+        self.urlRequest = urlRequest
+    }
     
 }
 
