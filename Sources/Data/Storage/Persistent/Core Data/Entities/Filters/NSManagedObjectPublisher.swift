6113d6df426df614a0524d8af3a3c60efb04ad80
Rich <richard.clements@image-plus.co.uk>
2021-08-25 14:32:01 +0100

    Create NSManagedObjectPublisher.swift

---
+++ Sources/Data/Storage/Persistent/Core Data/Entities/Filters/NSManagedObjectPublisher.swift
@@ -0,0 +1,38 @@
+import CoreData
+import Combine
+
+@available(iOS 13.0, tvOS 13.0, macOS 10.15, watchOS 6.0, *)
+enum NSManagedObjectFutureError: Error {
+    case noContext
+}
+
+@available(iOS 13.0, tvOS 13.0, macOS 10.15, watchOS 6.0, *)
+extension Optional where Wrapped == NSManagedObject {
+    
+    func future<Output>(_ closure: @escaping () throws -> Output) -> AnyPublisher<Output, Error> {
+        Future { [weak self] promise in
+            guard let context = self?.managedObjectContext else {
+                promise(.failure(NSManagedObjectFutureError.noContext))
+                return
+            }
+            context.perform {
+                do {
+                    let output = try closure()
+                    promise(.success(output))
+                } catch {
+                    promise(.failure(error))
+                }
+            }
+        }.eraseToAnyPublisher()
+    }
+    
+}
+
+@available(iOS 13.0, tvOS 13.0, macOS 10.15, watchOS 6.0, *)
+extension Array where Element == NSManagedObject {
+    
+    func future<Output>(_ closure: @escaping () throws -> Output) -> AnyPublisher<Output, Error> {
+        first.future(closure)
+    }
+    
+}
