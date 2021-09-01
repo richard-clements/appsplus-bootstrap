e75fa1fe0da54f63f1fdc3e9bb719ab9516854de
Rich <richard.clements@image-plus.co.uk>
2021-09-01 15:42:13 +0100

    Update MockAuthSessionProvider.swift

--- Sources/Testing/Mocks/Data/Network/MockAuthSessionProvider.swift
+++ Sources/Testing/Mocks/Data/Network/MockAuthSessionProvider.swift
@@ -3,41 +3,41 @@ import Foundation
 import Combine
 
 @available(iOS 13.0, tvOS 13.0, macOS 10.15, watchOS 6.0, *)
-class MockAuthSessionProvider<AuthToken: AuthTokenProtocol>: AuthSessionProvider {
+public class MockAuthSessionProvider<AuthToken: AuthTokenProtocol>: AuthSessionProvider {
     
-    var currentAuthSession: AuthToken?
-    var replacedAuthSession: AuthToken?
-    var didCallReplace = false
-    var replaceSuccess = false
-    var currentDeviceName: String?
-    var authSessionPublisherPassthroughSubject = PassthroughSubject<AuthToken?, Never>()
+    public var currentAuthSession: AuthToken?
+    public var replacedAuthSession: AuthToken?
+    public var didCallReplace = false
+    public var replaceSuccess = false
+    public var currentDeviceName: String?
+    public var authSessionPublisherPassthroughSubject = PassthroughSubject<AuthToken?, Never>()
     
-    func current() -> AnyAuthToken? {
+    public func current() -> AnyAuthToken? {
         currentAuthSession?.toAnyAuthToken()
     }
     
-    func current<T>(as type: T.Type) -> T? where T : AuthTokenProtocol {
+    public func current<T>(as type: T.Type) -> T? where T : AuthTokenProtocol {
         return currentAuthSession as? T
     }
     
-    func replace<T>(with authToken: T?) -> Bool where T : AuthTokenProtocol {
+    public func replace<T>(with authToken: T?) -> Bool where T : AuthTokenProtocol {
         didCallReplace = true
         replacedAuthSession = authToken as? AuthToken
         return replaceSuccess
     }
     
-    var deviceName: String {
+    public var deviceName: String {
         return currentDeviceName!
     }
     
-    func authSessionPublisher<T>(for type: T.Type) -> AnyPublisher<T?, Never> where T : AuthTokenProtocol {
+    public func authSessionPublisher<T>(for type: T.Type) -> AnyPublisher<T?, Never> where T : AuthTokenProtocol {
         authSessionPublisherPassthroughSubject
             .share()
             .map { $0 as? T }
             .eraseToAnyPublisher()
     }
     
-    func authSessionPublisher() -> AnyPublisher<AnyAuthToken?, Never> {
+    public func authSessionPublisher() -> AnyPublisher<AnyAuthToken?, Never> {
         authSessionPublisherPassthroughSubject
             .share()
             .map { $0?.toAnyAuthToken() }
