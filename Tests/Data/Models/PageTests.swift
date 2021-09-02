2c7aa7295a961ed66ec89fe900cdb91ed3439ef0
Rich <richard.clements@image-plus.co.uk>
2021-09-02 14:08:44 +0100

    Added page tests

---
+++ Tests/Data/Models/PageTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import AppsPlusData
+
+class PageTests: XCTestCase {
+    
+    func testMap() {
+        let page = Page(data: [1, 2, 3, 4, 5], meta: .init(currentPage: 1, to: 3))
+        let newPage = page.map(\.description)
+        XCTAssertEqual(["1", "2", "3", "4", "5"], newPage.data)
+        XCTAssertEqual(1, page.meta.currentPage)
+        XCTAssertEqual(3, page.meta.to)
+    }
+    
+}
