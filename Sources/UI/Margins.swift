c53ea182ec5156a770d7b5ee95732b87dd307937
Rich <richard.clements@image-plus.co.uk>
2021-10-21 07:36:01 +0100

    Added margin

---
+++ Sources/UI/Margins.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+extension CGFloat {
+    enum Margin {
+        case xSmall
+        case small
+        case medium
+        case large
+    }
+    
+    static func margin(_ x: Margin): CGFloat {
+        switch x {
+        case .xSmall:
+            return 5
+        case .small:
+            return 10
+        case .medium:
+            return 20
+        case .large:
+            return 30
+        }
+    }
+}
