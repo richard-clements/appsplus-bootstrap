c9b967086ceef1ce2203ba057305d07adc2ab85f
Rich <richard.clements@image-plus.co.uk>
2021-10-22 15:47:41 +0100

    Made empty state view public

--- Sources/UI/Components/Views/EmptyStateView.swift
+++ Sources/UI/Components/Views/EmptyStateView.swift
@@ -3,7 +3,7 @@
 import UIKit
 
 @available(iOS 13.0, tvOS 13.0, *)
-class EmptyStateView<Control: UIControl>: UIView {
+public class EmptyStateView<Control: UIControl>: UIView {
     
     public var image: UIImage? {
         get {
