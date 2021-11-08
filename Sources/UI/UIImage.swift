870d99d573c917241143060c95ee4f0967ab5c1f
Rich <richard.clements@image-plus.co.uk>
2021-11-08 14:33:09 +0000

    Update UIImage.swift

--- Sources/UI/UIImage.swift
+++ Sources/UI/UIImage.swift
@@ -2,20 +2,37 @@ import UIKit
 
 extension UIImage {
     
-    public func atSize(_ size: CGSize) -> UIImage {
+    public func atSize(_ size: CGSize, isVectorImage: Bool = false) -> UIImage {
         guard size.width > 0 && size.height > 0 else {
             return self
         }
         var newSize = self.size
-        if newSize.width > size.width {
-            let scaleFactor = size.width/newSize.width
-            newSize.width = size.width
-            newSize.height *= scaleFactor
-        }
-        if newSize.height > size.height {
-            let scaleFactor = size.height/newSize.height
-            newSize.height = size.height
-            newSize.width *= scaleFactor
+        if isVectorImage {
+            if newSize.width < size.width {
+                let scaleFactor = size.width/newSize.width
+                newSize.width = size.width
+                newSize.height *= scaleFactor
+            } else if newSize.width > size.width {
+                let scaleFactor = size.width/newSize.width
+                newSize.width = size.width
+                newSize.height *= scaleFactor
+            }
+            if newSize.height > size.height {
+                let scaleFactor = size.height/newSize.height
+                newSize.height = size.height
+                newSize.width *= scaleFactor
+            }
+        } else {
+            if newSize.width > size.width {
+                let scaleFactor = size.width/newSize.width
+                newSize.width = size.width
+                newSize.height *= scaleFactor
+            }
+            if newSize.height > size.height {
+                let scaleFactor = size.height/newSize.height
+                newSize.height = size.height
+                newSize.width *= scaleFactor
+            }
         }
         return UIGraphicsImageRenderer(size: newSize).image { _ in
             draw(in: CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height))
