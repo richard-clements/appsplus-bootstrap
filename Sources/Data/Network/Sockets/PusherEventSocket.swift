fd837ce6d568d56bb071f2008b589f4147af0279
Rich <richard.clements@image-plus.co.uk>
2022-03-24 15:59:42 +0000

    Fixed build error

--- Sources/Data/Network/Sockets/PusherEventSocket.swift
+++ Sources/Data/Network/Sockets/PusherEventSocket.swift
@@ -90,7 +90,7 @@ public class PusherWebSocket: EventSocket, PusherDelegate {
     public init(
         authenticator: Authenticator,
         endpoint: URL,
-        usesTLS: Bool
+        usesTLS: Bool,
         port: Int,
         versionNumber: String,
         urlSession: URLSession,
