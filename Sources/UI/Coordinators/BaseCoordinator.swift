2cd45d072f4c8237e6ed4c9880e48521f3eca6dc
Filipe Lopes Image Plus <filipe.lopes@image-plus.co.uk>
2025-01-16 11:19:39 +0000

    release added base coordinator

---
+++ Sources/UI/Coordinators/BaseCoordinator.swift
@@ -0,0 +1,31 @@
+#if canImport(UIKit) && (os(iOS) || os(tvOS) || os(macOS))
+
+import UIKit
+
+protocol Coordinator: AnyObject {
+    
+    var childCoordinators: [Coordinator] { get set }
+    func start()
+    
+}
+
+public class BaseCoordinator: Coordinator {
+    
+    var childCoordinators: [Coordinator] = []
+    
+    func start() {
+        // Implementation specific to the coordinator
+    }
+    
+    func addChildCoordinator(_ coordinator: Coordinator) {
+        childCoordinators.append(coordinator)
+    }
+    
+    func removeChildCoordinator(_ coordinator: Coordinator?) {
+        guard let coordinator = coordinator else { return }
+        childCoordinators.removeAll { $0 === coordinator }
+    }
+    
+}
+
+#endif
